03a70883b740b5eb41bbb46454de7f325a6bfe54
Mi <74912354+milehagen@users.noreply.github.com>
2021-08-10 14:05:22 +0200

    re-allocated html to JS and updated fake data to API
    took out daily-weather forecast from html, and added to JS

--- src/javascript.js
+++ src/javascript.js
@@ -4,23 +4,99 @@
 let currentDate = new Date();
 let h2 = document.querySelector("#current-date");
 let days= [
-    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+    "Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday", 
 ]
 let day= days[currentDate.getDay()];
 let months= [
-    "Dec", "Jan", "Feb", "Mar", "Apr","Jun", "Jul","Aug", "Sept", "Oct", "Nov",
+    "Dec", 
+    "Jan", 
+    "Feb", 
+    "Mar", 
+    "Apr",
+    "Jun", 
+    "Jul",
+    "Aug", 
+    "Sept", 
+    "Oct", 
+    "Nov",
 ];
 let month= months [currentDate.getMonth()];
 let date= currentDate.getDate();
 let hour=currentDate.getHours();
 let minutes= currentDate.getMinutes();
 
-
 h2.innerHTML= (`${day} | ${month} ${date} | ${hour}:${minutes}`);
 
+//Clean-up date
+
+function formatDay(timeStamp){
+    let date = new Date(timeStamp* 1000);
+    let day= date.getDay();
+    let days = [
+    "Sun", 
+    "Mon", 
+    "Tues", 
+    "Wed", 
+    "Thur", 
+    "Fri", 
+    "Sat",]
+
+return days[day];
+}
+
+//display-5-day-Weather-Forecast
+function displayForecast(response){
+    console.log(response.data.daily);
+    let forecast= response.data.daily;
+ let forecastElement=document.querySelector("#forecast");
+
+ let forecastHTML=`<div class="row">
+`;
+ forecast.forEach(function(forecastDay, index){
+    if (index < 6){
+    forecastHTML = 
+    forecastHTML +
+
+`
+      <div  class="col-2">
+        <p>
+
+       <div class="forecast-day"> ${formatDay(forecastDay.dt)}</div>
+        <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+        alt= "weather image"
+        />
+        <br>
+            <span class="minTemp">${Math.round(forecastDay.temp.min)}º</span>|<span class="maxTemp">${Math.round(forecastDay.temp.max)}º</span>
+           
+            </p>          
+    `;
+     }
+    forecastHTML= forecastHTML+`</div>`;
+ });
+
+    forecastElement.innerHTML=forecastHTML;
+}
+
+//retrieve-city-coordinates
+
+function getCoordinates(coordinates){
+    let lat =coordinates.lat;
+    let lon= coordinates.lon;
+    let units = "metric";
+    let apiKey="7bc66b8226689078b2ef89f11a04633c";
+    let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(displayForecast);
+}
+
 //show-user's-cityTemp
+
 function showUserTemperature(response){
-    console.log(response.data)
     let winSpeed = document.querySelector("#wind-speed");
     let humidity = document.querySelector("#humidity-percentage");
     let cityHeadline = document.querySelector("h1");
@@ -35,6 +111,8 @@ function showUserTemperature(response){
     displayCurrentDegrees.innerHTML=`${Math.round(response.data.main.temp)}º`;
     currentWeatherDescription.innerHTML= response.data.weather[0].description;
     weatherIcon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+    getCoordinates(response.data.coord);
 }
 //Search-city
 
@@ -73,3 +151,4 @@ let locationIcon = document.querySelector("#location-icon");
 locationIcon.addEventListener("click", fetchUsersLocation)
 
 search ("Oslo");
+
